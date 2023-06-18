2df898921ddf6796028ea97784105bda3041554e
rirefat <rirefat.official@gmail.com>
2023-06-18 21:44:04 +0600

    add some pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import './App.css'
 
 const App = () => {
   return (
-    <div className='app'>
+    <div>
       
     </div>
   );
---
+++ src/pages/DoctorProfile/DoctorProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DoctorProfile = () => {
+    return (
+        <div>
+            <h1>Doc profile</h1>
+        </div>
+    );
+};
+
+export default DoctorProfile;
