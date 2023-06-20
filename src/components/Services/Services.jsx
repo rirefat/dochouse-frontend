3abc194649274a40641b29c468d7f95b27d6f928
rirefat <rirefat.official@gmail.com>
2023-06-21 01:52:38 +0600

    done responsive service tab

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -45,27 +45,33 @@ const Services = () => {
                         <div className="tab-content">
                             <div className={toggleState === 1 ? "active-content content" : "content"}>
                                 <img className='rounded-xl' src={cavityProtection} alt="" />
-                                <h3 className='my-5 font-semibold text-3xl text-black'>Cavity Protection</h3>
-                                <p className='text-black mb-5'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error. doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve  dicta sunt explicabo. Sed ut perspiciatis unde dicta sunt explicabo. Sed ut perspiciatis undentore veritatis et quasi architecto beatae vitae </p>
-                                <p className='text-black mb-5'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error </p>
+                                <div>
+                                    <h3 className='my-5 font-semibold text-3xl text-black'>Cavity Protection</h3>
+                                    <p className='text-black mb-5'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error. doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve  dicta sunt explicabo. Sed ut perspiciatis unde dicta sunt explicabo. Sed ut perspiciatis undentore veritatis et quasi architecto beatae vitae </p>
+                                    <p className='text-black mb-5'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error </p>
 
-                                <Link to="appointment"><button className="btn btn-outline btn-secondary">Get Appointment</button></Link>
+                                    <Link to="appointment"><button className="btn btn-outline btn-secondary">Get Appointment</button></Link>
+                                </div>
                             </div>
                             <div className={toggleState === 2 ? "active-content content" : "content"}>
                                 <img className='rounded-xl' src={cosmeticDentisty} alt="" />
-                                <h3 className='my-5 font-semibold text-3xl text-black'>Cosmetic Dentisty</h3>
-                                <p className='text-black mb-5'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error. doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve  dicta sunt explicabo. Sed ut perspiciatis unde dicta sunt explicabo. Sed ut perspiciatis undentore veritatis et quasi architecto beatae vitae </p>
-                                <p className='text-black mb-5'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error </p>
+                                <div>
+                                    <h3 className='my-5 font-semibold text-3xl text-black'>Cavity Protection</h3>
+                                    <p className='text-black mb-5'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error. doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve  dicta sunt explicabo. Sed ut perspiciatis unde dicta sunt explicabo. Sed ut perspiciatis undentore veritatis et quasi architecto beatae vitae </p>
+                                    <p className='text-black mb-5'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error </p>
 
-                                <Link to="appointment"><button className="btn btn-outline btn-secondary">Get Appointment</button></Link>
+                                    <Link to="appointment"><button className="btn btn-outline btn-secondary">Get Appointment</button></Link>
+                                </div>
                             </div>
                             <div className={toggleState === 3 ? "active-content content" : "content"}>
                                 <img className='rounded-xl' src={oralSurgery} alt="" />
-                                <h3 className='my-5 font-semibold text-3xl text-black'>Oral Surgery</h3>
-                                <p className='text-black mb-5'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error. doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve  dicta sunt explicabo. Sed ut perspiciatis unde dicta sunt explicabo. Sed ut perspiciatis undentore veritatis et quasi architecto beatae vitae </p>
-                                <p className='text-black mb-5'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error </p>
+                                <div>
+                                    <h3 className='my-5 font-semibold text-3xl text-black'>Cavity Protection</h3>
+                                    <p className='text-black mb-5'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error. doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve  dicta sunt explicabo. Sed ut perspiciatis unde dicta sunt explicabo. Sed ut perspiciatis undentore veritatis et quasi architecto beatae vitae </p>
+                                    <p className='text-black mb-5'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error </p>
 
-                                <Link to="appointment"><button className="btn btn-outline btn-secondary">Get Appointment</button></Link>
+                                    <Link to="appointment"><button className="btn btn-outline btn-secondary">Get Appointment</button></Link>
+                                </div>
                             </div>
                         </div>
                     </div>
