a6ee87514f729d86e218dccadb29866d5e432e7c
rirefat <rirefat.official@gmail.com>
2023-06-20 18:08:54 +0600

    done upto footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import logo from '../../assets/images/logo.png';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is footer</h1>
-        </div>
+        <footer className="footer p-10 bg-[#F3F3F3] text-black">
+            <div>
+                <Link to="/"><img src={logo} alt="" /></Link>
+                <p className='w-96'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi impedit ducimus quam minima mollitia doloremque, expestias labore reiciendis qui at ipsam dolorem optio. Iure?</p>
+            </div>
+            <div>
+                <span className="footer-title">Services</span>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </div>
+        </footer>
     );
 };
 
