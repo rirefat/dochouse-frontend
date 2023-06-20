7a928a8f490eff558745cf5ba38c358ff2094761
rirefat <rirefat.official@gmail.com>
2023-06-20 18:01:19 +0600

    done info cards

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -9,16 +9,16 @@ const InfoCard = () => {
     const informations = [
         { icon: IoIosTime, title: "Opening Hours", details: "Open 9.00 am to 5.00pm Everyday", bg: "primary"},
         { icon: MdAddLocation, title: "Our Location", details: "Dhanmondi 17, Dhaka -1200, Bangladesh", bg: "secondary"},
-        { icon: BsFillTelephoneFill, title: "Contact Us", details: "+88 01750 00 00 00  +88 01750 00 00 00", bg: "primary"},
+        { icon: BsFillTelephoneFill, title: "Contact Us", details: "+88 01750 00 00 00 ", bg: "primary"},
     ];
     return (
-        <div className='info-cards flex justify-center gap-12'>
+        <div className='info-cards my-12'>
             {
                 informations.map((info, index) => (
-                    <div className="card w-96 bg-base-100 shadow-xl my-24 flex flex-row align-center" key={index}>
-                        <div className="icon text-5xl text-white"><info.icon></info.icon></div>
-                        <div className={`card-body rounded-2xl text-white`}>
-                            <h2 className="text-2xl font-semibold">{info.title}</h2>
+                    <div className="single-card rounded-lg shadow-xl text-white pt-10" key={index}>
+                        <div className="icon text-5xl mx-5"><info.icon></info.icon></div>
+                        <div className="card-details">
+                            <h3 className='text-2xl mb-2'>{info.title}</h3>
                             <p>{info.details}</p>
                         </div>
                     </div>
