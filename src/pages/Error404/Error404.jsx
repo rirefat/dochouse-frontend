8937dd1872b1f3eca06067c7acc3e9b4ce750fc2
rirefat <rirefat.official@gmail.com>
2023-06-21 20:59:45 +0600

    done responsive 404 page

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Error404.css';
+import error404 from '../../assets/images/page404.png';
+import { Link } from 'react-router-dom';
 
 const Error404 = () => {
     return (
-        <div>
-            <h1>error 404</h1>
+        <div className='not-found-page text-center'>
+            <h1 className='text-4xl mb-4 text-black font-semibold'>Sorry</h1>
+            <p className='text-black text-xl '>This page is not found</p>
+            <img src={error404} className='my-8' />
+            <Link to="/"><button className="btn btn-secondary">Back To Home</button></Link>
         </div>
     );
 };
